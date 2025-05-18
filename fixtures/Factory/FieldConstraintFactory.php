d86734220b713835a3f4695cfc9f0c25f8d8218c
David Pelletier-Ulrich <d@mztrix.me>
2025-05-18 19:38:38 +0200

    introducing and setup resource types

---
+++ fixtures/Factory/FieldConstraintFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the AstroBook project.
+ * (c) David Pelletier-Ulrich <d@mztrix.me>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dogstronauts\AstroBook\Fixtures\Factory;
+
+use Dogstronauts\AstroBook\Resources\Model\FieldConstraint;
+use Zenstruck\Foundry\ObjectFactory;
+
+/**
+ * @extends ObjectFactory<FieldConstraint>
+ */
+final class FieldConstraintFactory extends ObjectFactory
+{
+    public static function class(): string
+    {
+        return FieldConstraint::class;
+    }
+
+    protected function defaults(): array
+    {
+        return [
+            'class' => self::faker()->text(),
+            'options' => [],
+        ];
+    }
+}
---
+++ fixtures/Factory/FieldFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the AstroBook project.
+ * (c) David Pelletier-Ulrich <d@mztrix.me>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dogstronauts\AstroBook\Fixtures\Factory;
+
+use Dogstronauts\AstroBook\Resources\Model\Field;
+use Dogstronauts\AstroBook\Resources\Model\FieldType;
+use Zenstruck\Foundry\ObjectFactory;
+
+/**
+ * @extends ObjectFactory<Field>
+ */
+final class FieldFactory extends ObjectFactory
+{
+    public static function class(): string
+    {
+        return Field::class;
+    }
+
+    protected function defaults(): array
+    {
+        return [
+            'type' => self::faker()->randomElement(FieldType::cases()),
+            'label' => self::faker()->text(),
+            'constraints' => FieldConstraintFactory::new()->many(1),
+        ];
+    }
+}
