5e1cba48a23fc8464c5fc014bf240df8133c8bd8
David Pelletier-Ulrich <d@mztrix.me>
2025-10-20 19:40:16 +0200

    upgrade dependencies

--- config/packages/zenstruck_foundry.php
+++ config/packages/zenstruck_foundry.php
@@ -26,7 +26,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             'make_story' => [
                 'default_namespace' => 'Dogstronauts\AstroBook\Fixtures\Story',
             ],
-            'enable_auto_refresh_with_lazy_objects' => true,
+            //            in anticipation of the upgrade of zenstruck/foundry to 2.7. Currently a bug
+            //            'enable_auto_refresh_with_lazy_objects' => true,
         ]);
     }
 };
