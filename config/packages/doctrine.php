d29c5555834924bd543c36c0ac3078b2855804d9
David Pelletier-Ulrich <d@mztrix.me>
2025-11-08 16:52:40 +0100

    remove deprecated `report_fields_where_declared` config from Doctrine

--- config/packages/doctrine.php
+++ config/packages/doctrine.php
@@ -22,7 +22,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         'orm' => [
             'auto_generate_proxy_classes' => true,
             'enable_lazy_ghost_objects' => true,
-            'report_fields_where_declared' => true,
             'validate_xml_mapping' => true,
             'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
             'identity_generation_preferences' => [
